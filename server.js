da2847438a57010d5da691b5929bff42772f92f1
Hridoy Banik <hridoy.banik71@gmail.com>
2023-05-20 22:02:39 +0600

    add: user auth completed

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const data = require("./data/data");
 const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 const app = express();
+app.use(express.json());
 dotenv.config();
 
 app.get("/", (req, res) => {
@@ -11,7 +15,11 @@ app.get("/", (req, res) => {
 app.get("/api/chats", (req, res) => {
   res.send(data);
 });
+
+app.use("/api/user", userRoutes);
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
+  connectDB();
   console.log(`server started on port ${PORT}`);
 });
