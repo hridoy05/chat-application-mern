0a3d5d5c38d9d817e46f7e4caf75f749ff0ea488
Hridoy Banik <hridoy.banik71@gmail.com>
2023-05-21 11:39:25 +0600

    add: complete search user api with protected middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userController");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
+router.route("/").get(allUsers);
 router.route("/").post(registerUser);
 router.post("/login", authUser);
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 app.use(express.json());
 dotenv.config();
@@ -17,6 +18,9 @@ app.get("/api/chats", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
