43373658d218172fe160a600a921b072f652dbdc
Hridoy Banik <hridoy.banik71@gmail.com>
2023-05-22 22:18:56 +0600

    add chat router, add fetchChat functionality in controller

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 app.use(express.json());
@@ -18,6 +19,7 @@ app.get("/api/chats", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
